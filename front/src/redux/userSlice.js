20158767d8aa12b22c579ce55e092853bcafc267
DUrizarSis <daurizar5@gmail.com>
2024-01-28 21:35:19 -0600

    feat(front): getOne-getAll

--- front/src/redux/userSlice.js
+++ front/src/redux/userSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const URL = 'http://localhost:3001/api/'
+const URL = 'http://localhost:5000/api/'
 
 /// Thunk
 
