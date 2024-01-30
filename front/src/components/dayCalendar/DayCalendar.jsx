a65e3804c7a298e168a467d079fb0427d5ff6ae6
GomezGustavoA <gustavo_64_231@hotmail.com>
2024-01-30 11:40:17 -0300

    cambios en componentes minicalendar y day calendar

--- front/src/components/dayCalendar/DayCalendar.jsx
+++ front/src/components/dayCalendar/DayCalendar.jsx
@@ -9,32 +9,9 @@ const DayCalendar = () => {
       //Event state global
     const eventState = useSelector(state => state.events);
     //Events state
-    //   const events = eventState.events;
-      const events = [
-        {
-          title: 'Cambio de nombre',
-          start: new Date('2024-01-17T13:00'),
-          end: new Date('2024-01-19T12:00'),
-        },
-        {
-          title: 'prueba',
-          start: new Date('2024-01-17T13:00'),
-          end: new Date('2024-01-18T12:00'),
-        },
-        {
-          title: 'prueba',
-          start: new Date('2024-01-24T13:00'),
-          end: new Date('2024-01-25T12:00'),
-        },
-        {
-          title: 'dfgdfgdf',
-          start: new Date('2024-02-01T02:15:00.000Z'),
-          end: new Date('2024-02-01T02:18:00.000Z'),
-        },
-        // ... otros eventos
-      ];
-      console.log(events)
+      const events = eventState.events;
 
+    console.log(events)
     return (
         <div className={Styles.container}>
             <h1>Day calendar</h1>
--- front/src/components/miniCalendar/MiniCalendar.jsx
+++ front/src/components/miniCalendar/MiniCalendar.jsx
@@ -2,16 +2,20 @@ import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
 import Styles from "./miniCalendar.module.css";
+import { useSelector } from 'react-redux';
 
 function MiniCalendar() {
 
   const localizer = dayjsLocalizer(dayjs);
-
+  const eventState = useSelector(state => state.events);
+  const events = eventState.events;
   return (
     <div className={Styles.container}>
       <h1>My Calendar</h1>
       <Calendar
       localizer={localizer}
+      events={events}
+      views={['month']}
       />
 
     </div>
