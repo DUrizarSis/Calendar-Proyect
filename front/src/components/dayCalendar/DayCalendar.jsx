a620d4c0ea442d83d0a478490391cee09087a2a6
GomezGustavoA <gustavo_64_231@hotmail.com>
2024-01-30 21:35:59 -0300

    cambios mini - day - redux

--- front/src/components/dayCalendar/DayCalendar.jsx
+++ front/src/components/dayCalendar/DayCalendar.jsx
@@ -3,8 +3,13 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
 import Styles from "./dayCalendar.module.css";
 import { useSelector } from 'react-redux';
+import { useState } from "react";
 
 const DayCalendar = () => {
+const [date, setDate] = useState({});
+
+  const dateMini= useSelector(state=> state.eventMini.date)
+  console.log(dateMini)
     const localizer = dayjsLocalizer(dayjs);
       //Event state global
     const eventState = useSelector(state => state.events);
@@ -21,6 +26,7 @@ const DayCalendar = () => {
                 events={events}
                 startAccessor={(event) => dayjs(event.start).toDate()}
                 endAccessor={(event) => dayjs(event.end).toDate()}
+                date={dateMini}
             />
         </div>
     )
