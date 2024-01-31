c5a498a715cac2f8afd108498ab207d4391f1842
DUrizarSis <daurizar5@gmail.com>
2024-01-31 16:13:13 -0600

    feat(back) change color event

--- server/src/controllers/putEvent.js
+++ server/src/controllers/putEvent.js
@@ -3,9 +3,9 @@ const Event = require("../models/event");
  module.exports = async (req, res) => {
     try {
       const { id } = req.params;
-      const { title, description, start, end } = req.body;
+      const { title, description, start, end, color } = req.body;
   
-      const event = await Event.updateOne({ _id: id }, { $set: { title, start, end, description } });
+      const event = await Event.updateOne({ _id: id }, { $set: { title, start, end, description, color } });
   
       // Recupera el evento actualizado después de la actualización
       const updatedEvent = await Event.findById(id);
