94875aecd25901a2d1a23776b37f4ef226639dad
GomezGustavoA <gustavo_64_231@hotmail.com>
2024-01-31 14:21:34 -0300

    commit del redux day mini

--- front/src/components/miniCalendar/MiniCalendar.jsx
+++ front/src/components/miniCalendar/MiniCalendar.jsx
@@ -2,20 +2,40 @@ import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
 import Styles from "./miniCalendar.module.css";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { addEventMini } from "../../redux/eventMiniSlice";
+
 
 function MiniCalendar() {
-
+  const dispatch = useDispatch()
   const localizer = dayjsLocalizer(dayjs);
   const eventState = useSelector(state => state.events);
   const events = eventState.events;
+
+  const handleSelectSlot = ({ start, end, slots, action, bounds, box }) => {
+    const formattedDate = start.toISOString();
+    dispatch(addEventMini(formattedDate));
+
+  };
+  
+  const handleSelectEvent = (event, e) => {
+    const formattedDate = event.start;
+    dispatch(addEventMini(formattedDate));
+
+  };
+
   return (
+
+    
     <div className={Styles.container}>
 
       <Calendar
       localizer={localizer}
       events={events}
       views={['month']}
+      selectable
+      onSelectSlot={handleSelectSlot}
+      onSelectEvent={handleSelectEvent}
       />
 
     </div>
